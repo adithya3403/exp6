3ad72e6a0fa87a419ee8108a2252869bf90e462c
adithya3403 <a.dty.3403@gmail.com>
2023-06-05 16:36:40 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [count, setCount] = useState(0);
+    const [isNight, setNight] = useState(true);
+    return (
+        <div className={`box ${isNight ? 'dayLight' : ''}`}>
+            <h1>{isNight ? 'Night' : 'Morning'}</h1>
+            <button onClick={() => setNight(!isNight)}>Update</button>
+            <h1>Counter value = {count}</h1>
+            <button onClick={() => setCount(count + 1)}>Increase Counter</button>
+            <button onClick={() => setCount(count - 1)}>Decrease Counter</button>
+        </div>
+    );
 }
-
 export default App;
